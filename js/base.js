a11885298cf15d32759b9aa664f534d50e4ba19c
Daniel Whitney <whitneyd@southwestern.edu>
2020-08-10 20:13:46 -0500

    initial commit

---
+++ js/base.js
@@ -0,0 +1,1 @@
+// this file will hold all of our html elements that we're going to use for dom manipulation
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: ["./src/index.js"],
+  output: {
+    path: path.resolve(__dirname, "static"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: "./",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
